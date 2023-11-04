66b5fc09e8c9b02b182b2f43d75ae427665b2919
Nabi <nabisaheb7780@gmail.com>
2023-11-04 13:24:52 +0530

    Expense poject setup

--- backend.sh
+++ backend.sh
@@ -1,36 +1,37 @@
+
 echo -e "\e[31m enabling nodejs \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module disable nodejs -y $/tmp/expense.log
+dnf module enable nodejs:18 -y $/tmp/expense.log
 
 echo -e "\e[31m install nodejs \e[0m"
-dnf install nodejs -y
+dnf install nodejs -y $/tmp/expense.log
 
 echo -e "\e[31m copying the backend services \e[0m"
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service $/tmp/expense.log
 
 echo -e "\e[31m useradd \e[0m"
-useradd expense
+useradd expense $/tmp/expense.log
 
 echo -e "\e[31m created app directory \e[0m"
-mkdir /app
+mkdir /app $/tmp/expense.log
 
 echo -e "\e[31m downloaded the zip file \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip $/tmp/expense.log
 
 echo -e "\e[31m unzip the file \e[0m"
-cd /app
-unzip /tmp/backend.zip
+cd /app $/tmp/expense.log
+unzip /tmp/backend.zip $/tmp/expense.log
 
 echo -e "\e[31m installed dependenties  \e[0m"
-npm install
+npm install $/tmp/expense.log
 
 echo -e "\e[31m install mysql \e[0m"
-dnf install mysql -y
+dnf install mysql -y $/tmp/expense.log
 
 echo -e "\e[31m setup the passwords \e[0m"
-mysql -h mysql-dev.nadevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql-dev.nadevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql $/tmp/expense.log
 
 echo -e "\e[31m service started \e[0m"
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+systemctl daemon-reload $/tmp/expense.log
+systemctl enable backend $/tmp/expense.log
+systemctl restart backend $/tmp/expense.log
--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 echo -e "\e[31m disabled mysql \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y $/tmp/expense.log
 
 echo -e "\e[31m copied mysql.repo file \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo $/tmp/expense.log
 
 echo -e "\e[31m installed mysql \e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y $/tmp/expense.log
 
 echo -e "\e[31m stared services \e[0m"
-systemctl enable mysqld
-systemctl restart mysqld
+systemctl enable mysqld $/tmp/expense.log
+systemctl restart mysqld $/tmp/expense.log
 
 echo -e "\e[31m setup the password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 $/tmp/expense.log
