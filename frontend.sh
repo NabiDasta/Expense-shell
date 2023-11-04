a24b9ef2db76f10e761cb51670a6c297f8954b61
Nabi <nabisaheb7780@gmail.com>
2023-11-04 10:12:09 +0530

    Expense poject setup

--- frontend.sh
+++ frontend.sh
@@ -6,10 +6,11 @@ cp expense.conf /etc/nginx/default.d/expense.conf
 
 echo -e "\e[32m  Removed a file \e[0m"
 rm -rf /usr/share/nginx/html/*
+
 echo -e "\e[32m   Download a zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\"[32m unzip the file \e[0m"
+echo -e "\[32m unzip the file \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
