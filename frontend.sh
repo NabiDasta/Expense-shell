93e69b2ba8b97ba42d848d09f56d9c207f57b95a
Nabi <nabisaheb7780@gmail.com>
2023-11-04 10:19:15 +0530

    Expense poject setup

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[32m Installing Nginx \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>tmp/expense.log
 
 echo -e "\e[32m copy file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>tmp/expense.log
 
-echo -e "\e[32m  Removed a file \e[0m"
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[32m Removed a file \e[0m"
+rm -rf /usr/share/nginx/html/* &>>tmp/expense.log
 
-echo -e "\e[32m   Download a zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[32m Download a zip file \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>tmp/expense.log
 
 echo -e "\e[32m unzip the file \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>tmp/expense.log
+unzip /tmp/frontend.zip &>>tmp/expense.log
 
 echo -e "\32m start the nginx \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>tmp/expense.log
+systemctl restart nginx &>>tmp/expense.log
