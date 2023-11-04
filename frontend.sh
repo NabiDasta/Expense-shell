23a917ae1d595d56e6f636c92750a88867add999
Nabi <nabisaheb7780@gmail.com>
2023-11-04 12:53:06 +0530

    Expense poject setup

--- frontend.sh
+++ frontend.sh
@@ -3,20 +3,26 @@ color="\e[33m"
 
 echo -e "${color} Installing Nginx \e[0m"
 dnf install nginx -y &>>$log_file
+echo $?
 
 echo -e "${color} copy file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $?
 
 echo -e "${color} Removed a file \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
 echo -e "${color} Download a zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
 
 echo -e "${color} unzip the file \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
+echo $?
 
 echo -e "${color} start the nginx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
+echo $?
