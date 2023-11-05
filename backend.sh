8b2da75a5fff18e9c58c89d02cc655133fc502c5
Nabi <nabisaheb7780@gmail.com>
2023-11-05 08:32:22 +0530

    Expense poject setup

--- backend.sh
+++ backend.sh
@@ -26,12 +26,15 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
+id expense &>>$log_file
+if [ $? -ne 0 ]; then
 echo -e "${color} useradd \e[0m"
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 fi
 
 echo -e "${color} created app directory \e[0m"
