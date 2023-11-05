64bdc487b4184332e5bd290c7372519146290b82
Nabi <nabisaheb7780@gmail.com>
2023-11-05 08:03:58 +0530

    Expense poject setup

--- backend.sh
+++ backend.sh
@@ -10,7 +10,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m install nodejs \e[0m"
+echo -e "${color} install nodejs \e[0m"
 dnf install nodejs -y &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -18,7 +18,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m copying the backend services \e[0m"
+echo -e "${color} copying the backend services \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -26,7 +26,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m useradd \e[0m"
+echo -e "${color} useradd \e[0m"
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -34,7 +34,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m created app directory \e[0m"
+echo -e "${color} created app directory \e[0m"
 mkdir /app &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -42,7 +42,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m Deleted app directory \e[0m"
+echo -e "${color} Deleted app directory \e[0m"
 rm -rf /app/* &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -50,7 +50,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m downloaded the zip file \e[0m"
+echo -e "${color} downloaded the zip file \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -58,7 +58,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m unzip the file \e[0m"
+echo -e "${color} unzip the file \e[0m"
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
 if [ $? -eq 0 ]; then
@@ -67,7 +67,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m installed dependenties  \e[0m"
+echo -e "${color} installed dependenties  \e[0m"
 npm install &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -75,7 +75,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m install mysql \e[0m"
+echo -e "${color} install mysql \e[0m"
 dnf install mysql -y &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -83,7 +83,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m setup the passwords \e[0m"
+echo -e "${color} setup the passwords \e[0m"
 mysql -h mysql-dev.nadevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
@@ -91,7 +91,7 @@ else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "\e[31m service started \e[0m"
+echo -e "${color} service started \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
